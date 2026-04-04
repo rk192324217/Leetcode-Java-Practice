5f97e73e180222bca5d83632439fbd06c4f37e73
rk192324217 <srk8115939@gmail.com>
2026-04-04 22:30:47 +0530

    04.04.2026

---
+++ Java/83_Evaluate Reverse Polish Notation (150).java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack <Integer> stk = new Stack<>();
+        for(String i : tokens){
+            if(i.equals("+")){
+                stk.push(stk.pop()+ stk.pop());
+            }
+            else if(i.equals("-")){
+                int b = stk.pop();
+                int a= stk.pop();
+                stk.push(a-b);
+            }
+            else if(i.equals("*")){
+                stk.push(stk.pop()*stk.pop());
+                
+            }
+            else if(i.equals("/")){
+                int b = stk.pop();
+                int a= stk.pop();
+                stk.push(a/b);
+            }
+            else{
+                stk.push(Integer.parseInt(i));
+            }
+        }
+        return stk.pop();
+    }
+}
