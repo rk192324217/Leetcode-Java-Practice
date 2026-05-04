8f02642e9e075e76f7abd163742f8cbbb0a2d234
rk192324217 <srk8115939@gmail.com>
2026-05-04 12:34:33 +0530

    04.05.2026

--- Java/28_Length of Last Word (58).java
+++ Java/28_Length of Last Word (58).java
@@ -1,11 +1,25 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         int res=0;
+//         s=s.trim();
+//         for (int i = s.length()-1 ; i>=0;i--){
+//             if(s.charAt(i)==' ') break;
+//             res++;
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public int lengthOfLastWord(String s) {
-        int res=0;
-        s=s.trim();
-        for (int i = s.length()-1 ; i>=0;i--){
-            if(s.charAt(i)==' ') break;
-            res++;
+        int res=0,i=s.length()-1;
+        while(0<=i && s.charAt(i)==' '){
+            i--;
         }
+        while(i>=0 && s.charAt(i)!=' '){
+            res++;
+            i--;
+        }
+        
         return res;
     }
 }
