6f2419566f7ef6e015a3a614d5a5940bbce656e2
rk192324217 <srk8115939@gmail.com>
2026-07-25 17:50:27 +0530

    25.07.2026

---
+++ Java/LeetCode 75/Array and String/151 Reverse Words in a String.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder res=new StringBuilder();
+        int i =s.length()-1;
+        while(i>=0){
+            while(i>=0 && s.charAt(i)==' '){i--;}
+            if(i<0){break;}
+            int end=i;
+            while(i>=0 && s.charAt(i)!=' '){
+                i--;
+            }
+            if(res.length()>0){res.append(' ');}
+            res.append(Substring(i+1,end,s));
+            
+        }
+        return res.toString();
+    }
+    public String Substring(int f,int l,String s){
+        StringBuilder substr=new StringBuilder();
+        for(int i =f;i<=l;i++){
+            substr.append(s.charAt(i));
+        }
+        return substr.toString();
+    }
+}
