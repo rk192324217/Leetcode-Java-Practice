9ad21fff85722e55bbdf05f65b4bd8b8cdf828f0
rk192324217 <srk8115939@gmail.com>
2026-07-27 14:11:28 +0530

    27.07.2026

---
+++ Java/LeetCode 75/Daily Challenge/1464 Maximum Product of Two Elements in an Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int m1=0,m2=0;
+        for(int i :nums){
+            if(i>m1){
+                m2=m1;
+                m1=i;
+            }
+            else if (i>m2){
+                m2=i;
+            }
+        }
+        return (m1-1)*(m2-1);
+    }
+}
